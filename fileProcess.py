0fd147586e558066964e9e32bc2ff90fec7afde3
yangpiao <502023290044@smail.nju.edu.cn>
2025-09-30 03:27:49 -0400

    add function read txt into list

--- fileProcess.py
+++ fileProcess.py
@@ -1,12 +1,14 @@
 import pickle
 import json
 
-# all_sta_dict=fileProcess.loadDict(fpath='../result/station.pickle')
 
 def hello():
 	print('fileProcess.py is imported successfully')
 
 def loadDict(fpath):
+    '''
+    load pickle or json file into dict data
+    '''
     if fpath.endswith("pickle"):
         with open(fpath, 'rb') as file:
             my_dict = pickle.load(file)
@@ -16,10 +18,45 @@ def loadDict(fpath):
     return my_dict
 
 
-# fileProcess.dict2file(filename='../result/resp',dict_output=resp)
-def dict2file(filename,dict_output):
+def dict2pickle(filename,dict_out):
+    '''
+    output dict to pickle file
+    '''
+
     if not filename.endswith('.pickle'):
         filename=filename+'.pickle'
 
     with open(filename, 'wb') as file:
-        pickle.dump(dict_output, file)
+        pickle.dump(dict_out, file)
+
+def loadLine2list(fpath,skip_empty=True,strip=True,comment=True):
+    """
+    Read a file line by line into a list.
+    
+    Parameters
+    ----------
+    fpath : str
+        Path to the file.
+    strip : bool, default True
+        Whether to strip whitespace (like '\n') from each line.
+    skip_empty : bool, default True
+        Whether to skip empty lines.
+    comment:
+        skip lines begining with #
+                
+    Returns
+    -------
+    list of str
+        List containing lines from the file.
+    """
+    lines = []
+    with open(fpath, 'r', encoding='utf-8') as f:
+        for line in f:
+            if line[0]=='#' and comment:
+                continue
+            if strip:
+                line = line.strip()
+            if skip_empty and not line:
+                continue
+            lines.append(line)
+    return lines
